a038db3da6045985fce102bcc3cfccc3a630a75f
nimri7856nimoo-cmyk <nimri.7856.nimoo@gmail.com>
2026-05-13 12:15:43 +0500

    Updated stage1 scraper flow

---
+++ flows/stage1_scraper.py
@@ -0,0 +1,22 @@
+from prefect import flow
+
+from tasks.call_apify_actor import call_apify_actor
+from tasks.write_raw_docs import write_raw_docs
+from tasks.process_posts import process_posts
+
+
+@flow
+def instagram_pipeline(username: str):
+
+    posts = call_apify_actor(username)
+
+    write_raw_docs(posts)
+
+    process_posts()
+
+    print("Pipeline completed successfully")
+
+
+if __name__ == "__main__":
+
+    instagram_pipeline("nike")
